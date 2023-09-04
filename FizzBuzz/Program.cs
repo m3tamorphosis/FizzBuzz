b20b09dff6fcf01a512b339f52557abec0c636f6
StewartC <StewartC@IT08637>
2023-09-04 09:28:00 +0100

    Add project files.

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                bool fizz = i % 3 == 0 || i.ToString().Contains("3");
+                bool buzz = i % 5 == 0 || i.ToString().Contains("5");
+                if (fizz && buzz)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (fizz) { Console.WriteLine("Fizz"); }
+                else if (buzz) { Console.WriteLine("Buzz"); }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
